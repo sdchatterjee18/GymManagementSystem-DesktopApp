2197688641f594cf0b3ca6f3ed01cba001cd95ef
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-05-31 23:11:23 +0530

    Update RegistrationFees.sql

--- Database/InitialQueries/CREATE_TABLE/RegistrationFees.sql
+++ Database/InitialQueries/CREATE_TABLE/RegistrationFees.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE tblRegistrationFees(
-       RegistrationFeesId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+       RegistrationFeesId INT PRIMARY KEY NOT NULL IDENTITY(1,1),
        FeeAmount DECIMAL(10,2) CHECK(FeeAmount >= 0) NOT NULL,
        IsActive BIT DEFAULT(1) NOT NULL,
        CreatedAt DATETIME 	DEFAULT(GETDATE()) NOT NULL
