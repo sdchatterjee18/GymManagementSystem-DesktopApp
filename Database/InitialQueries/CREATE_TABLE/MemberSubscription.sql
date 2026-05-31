800063791aec7b2d81c5799cecc78c6982a08c92
Priya Chowdhury <priyachowdhury528@gmail.com>
2026-05-31 13:58:15 +0530

    Create MemberSubscription  table

---
+++ Database/InitialQueries/CREATE_TABLE/MemberSubscription.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tblMemberSubscription (
+    MemberSubscriptionId INT IDENTITY(1,1) PRIMARY KEY,
+    MemberId INT NOT NULL,
+    MembershipPlanId INT NOT NULL,
+    StartDate DATE NOT NULL,
+    ExpiryDate DATE NOT NULL,
+    IsActive BIT NOT NULL DEFAULT(1),
+    CONSTRAINT FK_tblMemberSubscription_MemberIdtblMember FOREIGN KEY (MemberId) REFERENCES tblMember(MemberId),
+    CONSTRAINT FK_tblMemberSubscription_MembershipPlanIdtblMembershipPlan FOREIGN KEY (MembershipPlanId) REFERENCES tblMembershipPlans(MembershipPlanId)
+);
