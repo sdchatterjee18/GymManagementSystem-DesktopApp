492d3739c73519a63f165c95495ae2b9ee427539
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-05-31 12:58:07 +0530

    EmployeeTable

---
+++ Database/InitialQueries/CREATE_TABLE/Employee.sql
@@ -0,0 +1,18 @@
+CREATE TABLE tblEmployee
+(
+    EmployeeId INT PRIMARY KEY IDENTITY(1,1),
+    FirstName VARCHAR(150) NOT NULL,
+    MiddleName VARCHAR(50) NULL,
+    LastName VARCHAR(50) NOT NULL,
+    GenderId INT NOT NULL,
+    PhoneNo VARCHAR(20) UNIQUE NOT NULL,
+    EmailId VARCHAR(150) UNIQUE NOT NULL,
+    JoiningDate DATE NOT NULL DEFAULT(GETDATE()),
+    IsActive BIT NOT NULL DEFAULT(1),
+    RoleName VARCHAR(100) NOT NULL,
+    BankAccountNo VARCHAR(50) UNIQUE NOT NULL,
+    CONSTRAINT FK_tblEmployee_tblGender
+        FOREIGN KEY (GenderId)
+        REFERENCES tblGender(GenderId)
+);
+
