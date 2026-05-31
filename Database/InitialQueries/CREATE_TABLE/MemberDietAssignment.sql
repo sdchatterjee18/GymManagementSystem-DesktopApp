a8c3aa2d0b7c202a2d298a226d728d32febc2c12
Anshu_Dagur <164508557+Anshudagur@users.noreply.github.com>
2026-05-31 12:23:33 +0530

    Create tblMemberDietAssignment

--- Database/InitialQueries/CREATE_TABLE/MemberDietAssignment.sql
+++ Database/InitialQueries/CREATE_TABLE/MemberDietAssignment.sql
@@ -1,16 +1,14 @@
 -- Creating Table tblMemberDietAssignment
 Create Table tblMemberDietAssignment(
 	DietAssignmentId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	MemberId INT FK_MemberIdtblMemberMemberId Foreign Key(MemberId) References tblMember(MemberId) NOT NULL,
-	DietPlanId INT FK_DietPlanIdtblDietPlansDietPlanId Foreign Key(DietPlanId) References tblDietPlans(DietPlanId) NOT NULL,
+	MemberId INT NOT NULL,
+	DietPlanId INT NOT NULL,
 	AssignDate Date NOT NULL DEFAULT(GETDATE()),
-	IsActive BIT NOT NULL DEFAULT(1)
-)
-
--- Add Foregin Key : 
---										FK_MemberIdtblMemberMemberId
--- tblMemberDietAssignment(MemberId) --------------------------------> tblMember(MemberId)
-
--- Add Foregin Key : 
---										FK_DietPlanIdtblDietPlansDietPlanId
--- tblMemberDietAssignment(DietPlanId) ------------------------------------> tblDietPlans(DietPlanId)
+	IsActive BIT NOT NULL DEFAULT(1),
+	CONSTRAINT FK_MemberIdtblMemberMemberId 
+		FOREIGN KEY (MemberId) 
+		REFERENCES tblMember(MemberId),
+	CONSTRAINT FK_DietPlanIdtblDietPlansDietPlanId 
+		FOREIGN KEY (DietPlanId) 
+		REFERENCES tblDietPlans(DietPlanId)
+);
