a6a71760d9e8293b45f4396c67f79fa4393bacd5
Priya Chowdhury <priyachowdhury528@gmail.com>
2026-05-31 11:33:18 +0530

    Update Admin table

--- Database/InitialQueries/CREATE_TABLE/Admin.sql
+++ Database/InitialQueries/CREATE_TABLE/Admin.sql
@@ -1,14 +1,15 @@
-CREATE TABLE tblAdmins (
+CREATE TABLE tblAdmin(
     AdminId INT IDENTITY(1,1) PRIMARY KEY,
     EmployeeId INT NOT NULL,
     SuperAdminID INT NOT NULL,
     UserName VARCHAR(100) NOT NULL UNIQUE,
     PasswordHash VARCHAR(255) NOT NULL,
     LastLogin DATETIME NULL,
-    CONSTRAINT FK_EmployeeIdtblEmployeesEmployeeId
+    CONSTRAINT FK_EmployeeIdtblEmployeeEmployeeId
         FOREIGN KEY (EmployeeId)
-        REFERENCES tblEmployees(EmployeeId),
-    CONSTRAINT FK_SuperAdminIDtblSuperAdminsSuperAdminID
+        REFERENCES tblEmployee(EmployeeId),
+    CONSTRAINT FK_SuperAdminIDtblSuperAdminSuperAdminID
         FOREIGN KEY (SuperAdminID)
-        REFERENCES tblSuperAdmins(SuperAdminID)
+        REFERENCES tblSuperAdmin(SuperAdminID)
 );
+
