4e02ca9533f7452f60c806185b2eec1612a9943b
Priya Chowdhury <priyachowdhury528@gmail.com>
2026-05-31 15:04:49 +0530

    Update MemberAttendance Table

--- Database/InitialQueries/CREATE_TABLE/MemberAttendance.sql
+++ Database/InitialQueries/CREATE_TABLE/MemberAttendance.sql
@@ -3,6 +3,10 @@ CREATE TABLE tblMemberAttendance (
     MemberId INT NOT NULL,
     ShiftId INT NOT NULL,
     AttendanceDate DATETIME NOT NULL DEFAULT(GETDATE()),
-    CONSTRAINT FK_tblMemberAttendance_MemberIdtblMember FOREIGN KEY (MemberId) REFERENCES tblMember(MemberId),
-    CONSTRAINT FK_tblMemberAttendance_ShiftIdtblShift FOREIGN KEY (ShiftId) REFERENCES tblShift(ShiftId)
+    CONSTRAINT FK_tblMemberAttendance_MemberIdtblMember
+		FOREIGN KEY (MemberId)
+		REFERENCES tblMember(MemberId),
+    CONSTRAINT FK_tblMemberAttendance_ShiftIdtblShift 
+		FOREIGN KEY (ShiftId)
+		REFERENCES tblShift(ShiftId)
 );
