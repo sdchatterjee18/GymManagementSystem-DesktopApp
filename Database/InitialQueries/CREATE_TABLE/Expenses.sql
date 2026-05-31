5dcf8b3d51b66be7da2156bfae1d28aa2437e9ca
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-05-31 20:47:48 +0530

    Expenses table created

---
+++ Database/InitialQueries/CREATE_TABLE/Expenses.sql
@@ -0,0 +1,12 @@
+CREATE TABLE tblExpense
+(
+    ExpenseId INT  PRIMARY KEY IDENTITY(1,1) not null,
+    ExpenseCategoryId INT NOT NULL,
+    ExpenseAmount DECIMAL(10,2) NOT NULL
+        CHECK (ExpenseAmount >= 0),
+    ExpenseDate DATE NOT NULL,
+    Notes VARCHAR(MAX) NULL,
+    CONSTRAINT FK_tblExpense_ExpenseCategoryIdtblExpenseCategory
+        FOREIGN KEY (ExpenseCategoryId)
+        REFERENCES tblExpensesCategories(ExpenseCategoryId)
+);
