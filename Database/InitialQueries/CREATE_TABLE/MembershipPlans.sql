78573909e87b097b14777f685e963b20b1426589
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-05-31 13:03:12 +0530

    Update MembershipPlans.sql

--- Database/InitialQueries/CREATE_TABLE/MembershipPlans.sql
+++ Database/InitialQueries/CREATE_TABLE/MembershipPlans.sql
@@ -1,6 +1,6 @@
 CREATE TABLE tblMembershipPlans
 (
-    MembershipPlanId INT PRIMARY KEY IDENTITY(1,1),
+    MembershipPlanId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
     MembershipPlanName VARCHAR(100) NOT NULL,
     PlanType VARCHAR(50) NOT NULL,
     DurationInDays INT NOT NULL
