a3a2b78287386417c51ed9137d2396a4aaa160d1
Arindam Sahoo <arindamsahoo302@gmail.com>
2026-06-02 02:59:54 +0530

    Insert Data Into WorkoutPlans Table

---
+++ Database/InitialQueries/INSERT_DATA/InsertDataTestingWorkoutPlansTable.sql
@@ -0,0 +1,34 @@
+EXEC spInsertDataIntoWorkoutPlansTable
+    @WorkoutName = 'Monday Workout',
+    @Description = 'Chest and Triceps Exercises';
+GO
+
+EXEC spInsertDataIntoWorkoutPlansTable
+    @WorkoutName = 'Tuesday Workout',
+    @Description = 'Back and Biceps Exercises';
+GO
+
+EXEC spInsertDataIntoWorkoutPlansTable
+    @WorkoutName = 'Wednesday Workout',
+    @Description = 'Shoulders and Abs Exercises';
+GO
+
+EXEC spInsertDataIntoWorkoutPlansTable
+    @WorkoutName = 'Thursday Workout',
+    @Description = 'Legs and Calves Exercises';
+GO
+
+EXEC spInsertDataIntoWorkoutPlansTable
+    @WorkoutName = 'Friday Workout',
+    @Description = 'Chest and Triceps Advanced Exercises';
+GO
+
+EXEC spInsertDataIntoWorkoutPlansTable
+    @WorkoutName = 'Saturday Workout',
+    @Description = 'Back, Biceps and Forearms Exercises';
+GO
+
+EXEC spInsertDataIntoWorkoutPlansTable
+    @WorkoutName = 'Sunday Workout',
+    @Description = 'Full Body Workout, Cardio and Stretching';
+GO
