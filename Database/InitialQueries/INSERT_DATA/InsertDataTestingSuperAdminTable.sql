973c983a9ec9476ede8b407bbd42fb2ca935d2d4
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-06-03 00:16:47 +0530

    Delete InsertDataTestingSuperAdminTable.sql

--- Database/InitialQueries/INSERT_DATA/InsertDataTestingSuperAdminTable.sql
+++
@@ -1,7 +0,0 @@
-EXEC spInsertDataIntoSuperAdminTable
-    @UserName = 'admin1',
-    @PasswordHash = 'Admin@123',
-    @EmailId = 'admin1@gmail.com',
-    @PhoneNumber = '9876543210';
-
-SELECT *FROM tblSuperAdmin
