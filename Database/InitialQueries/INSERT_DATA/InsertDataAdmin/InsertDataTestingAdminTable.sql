5224f102a0d40e4e41e87edb8a343caeff621d1a
Priya Chowdhury <priyachowdhury528@gmail.com>
2026-06-03 00:41:55 +0530

    smaller update

--- Database/InitialQueries/INSERT_DATA/InsertDataAdmin/InsertDataTestingAdminTable.sql
+++ Database/InitialQueries/INSERT_DATA/InsertDataAdmin/InsertDataTestingAdminTable.sql
@@ -6,24 +6,24 @@ EXEC spInsertDataIntoAdminTable
 
 EXEC spInsertDataIntoAdminTable
     @EmployeeId = 2,
-    @SuperAdminID = 2,
+    @SuperAdminID = 1,
     @UserName = 'Sourav_Admin1',
     @PasswordHash = 'Sourav@123';
 
 EXEC spInsertDataIntoAdminTable
     @EmployeeId = 3,
-    @SuperAdminID = 3,
+    @SuperAdminID = 1,
     @UserName = 'Neha_Admin1',
     @PasswordHash = 'Neha@123';
 
 EXEC spInsertDataIntoAdminTable
-    @EmployeeId = 1,
+    @EmployeeId = 4,
     @SuperAdminID = 2,
     @UserName = 'Rahul_Admin2',
     @PasswordHash = 'Rahul@124';
 
 EXEC spInsertDataIntoAdminTable
-    @EmployeeId = 2,
-    @SuperAdminID = 3,
+    @EmployeeId = 4,
+    @SuperAdminID = 1,
     @UserName = 'Sourav_Admin2',
     @PasswordHash = 'Sourav@124';
