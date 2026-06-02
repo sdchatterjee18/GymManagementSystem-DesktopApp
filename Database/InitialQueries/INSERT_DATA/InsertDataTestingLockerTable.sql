4ef9fb19bd7fce9040d2f1c34c8d50474e7232f8
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-06-02 23:46:06 +0530

    Update InsertDataTestingLockerTable.sql

--- Database/InitialQueries/INSERT_DATA/InsertDataTestingLockerTable.sql
+++ Database/InitialQueries/INSERT_DATA/InsertDataTestingLockerTable.sql
@@ -10,4 +10,44 @@ EXEC spInsertDataIntoLockerTable
 @LockerNo = 'L003',
 @LockerStatus = 'Maintenance'
 
+EXEC spInsertDataIntoLockerTable
+    @LockerNo = 'L004',
+    @LockerStatus = 'Available';
+
+EXEC spInsertDataIntoLockerTable
+    @LockerNo = 'L005',
+    @LockerStatus = 'Occupied';
+
+EXEC spInsertDataIntoLockerTable
+    @LockerNo = 'L006',
+    @LockerStatus = 'Maintenance';
+
+EXEC spInsertDataIntoLockerTable
+    @LockerNo = 'L007',
+    @LockerStatus = 'Available';
+
+EXEC spInsertDataIntoLockerTable
+    @LockerNo = 'L008',
+    @LockerStatus = 'Occupied';
+
+EXEC spInsertDataIntoLockerTable
+    @LockerNo = 'L009',
+    @LockerStatus = 'Maintenance';
+
+EXEC spInsertDataIntoLockerTable
+    @LockerNo = 'L010',
+    @LockerStatus = 'Available';
+
+EXEC spInsertDataIntoLockerTable
+    @LockerNo = 'L011',
+    @LockerStatus = 'Occupied';
+
+EXEC spInsertDataIntoLockerTable
+    @LockerNo = 'L012',
+    @LockerStatus = 'Maintenance';
+
+EXEC spInsertDataIntoLockerTable
+    @LockerNo = 'L013',
+    @LockerStatus = 'Available';
+
 SELECT *FROM tblLocker
