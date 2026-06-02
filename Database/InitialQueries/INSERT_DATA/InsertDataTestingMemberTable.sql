135f443197ddba8294d375b6d7d4016a3701a4d8
Priya Chowdhury <priyachowdhury528@gmail.com>
2026-06-02 19:35:58 +0530

    Inserted data into InsertDataTestingMemberTable.sql

---
+++ Database/InitialQueries/INSERT_DATA/InsertDataTestingMemberTable.sql
@@ -0,0 +1,107 @@
+EXEC spInsertDataIntoMemberTable
+@FirstName='Amit',
+@MiddleName='Kumar',
+@LastName='Das',
+@GenderId=1,
+@PhoneNo='9000000001',
+@Email_Id='amit@gmail.com',
+@City='Kolkata',
+@District='Kolkata',
+@State='West Bengal',
+@EmergencyContact='9000000101'
+
+EXEC spInsertDataIntoMemberTable
+@FirstName='Priya',
+@MiddleName='Rani',
+@LastName='Paul',
+@GenderId=2,
+@PhoneNo='9000000002',
+@Email_Id='priya@gmail.com',
+@City='Howrah',
+@District='Howrah',
+@State='West Bengal',
+@EmergencyContact='9000000102'
+
+EXEC spInsertDataIntoMemberTable
+@FirstName='Rahul',
+@MiddleName='Kumar',
+@LastName='Sharma',
+@GenderId=1,
+@PhoneNo='9000000003',
+@Email_Id='rahul@gmail.com',
+@City='Durgapur',
+@District='Bardhaman',
+@State='West Bengal',
+@EmergencyContact='9000000103'
+
+EXEC spInsertDataIntoMemberTable
+@FirstName='Neha',
+@MiddleName='',
+@LastName='Roy',
+@GenderId=2,
+@PhoneNo='9000000004',
+@Email_Id='neha@gmail.com',
+@City='Siliguri',
+@District='Darjeeling',
+@State='West Bengal',
+@EmergencyContact='9000000104'
+
+EXEC spInsertDataIntoMemberTable
+@FirstName='Alex',
+@MiddleName='',
+@LastName='Sen',
+@GenderId=3,
+@PhoneNo='9000000005',
+@Email_Id='alex@gmail.com',
+@City='Kalyani',
+@District='Nadia',
+@State='West Bengal',
+@EmergencyContact='9000000105'
+
+EXEC spInsertDataIntoMemberTable
+@FirstName='Sourav',
+@MiddleName='Kumar',
+@LastName='Ghosh',
+@GenderId=1,
+@PhoneNo='9000000006',
+@Email_Id='sourav@gmail.com',
+@City='Asansol',
+@District='Bardhaman',
+@State='West Bengal',
+@EmergencyContact='9000000106'
+
+EXEC spInsertDataIntoMemberTable
+@FirstName='Ananya',
+@MiddleName='',
+@LastName='Dey',
+@GenderId=2,
+@PhoneNo='9000000007',
+@Email_Id='ananya@gmail.com',
+@City='Malda',
+@District='Malda',
+@State='West Bengal',
+@EmergencyContact='9000000107'
+
+EXEC spInsertDataIntoMemberTable
+@FirstName='Rakesh',
+@MiddleName='Kumar',
+@LastName='Yadav',
+@GenderId=1,
+@PhoneNo='9000000008',
+@Email_Id='rakesh@gmail.com',
+@City='Krishnanagar',
+@District='Nadia',
+@State='West Bengal',
+@EmergencyContact='9000000108'
+
+EXEC spInsertDataIntoMemberTable
+@FirstName='Maya',
+@MiddleName='',
+@LastName='Karmakar',
+@GenderId=2,
+@PhoneNo='9000000009',
+@Email_Id='maya@gmail.com',
+@City='Bankura',
+@District='Bankura',
+@State='West Bengal',
+@EmergencyContact='9000000109'
