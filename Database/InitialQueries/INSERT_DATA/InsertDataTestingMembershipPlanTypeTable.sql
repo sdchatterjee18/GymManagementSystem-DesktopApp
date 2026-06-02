cf98ac117b44f76aeb28a4dfe81c07c9a91e7958
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-06-03 00:16:42 +0530

    Delete InsertDataTestingMembershipPlanTypeTable.sql

--- Database/InitialQueries/INSERT_DATA/InsertDataTestingMembershipPlanTypeTable.sql
+++
@@ -1,9 +0,0 @@
-EXEC spInsertDataIntoMembershipPlanTypeTable @PlanType = 'Monthly';
-
-EXEC spInsertDataIntoMembershipPlanTypeTable @PlanType = 'Quarterly';
-
-EXEC spInsertDataIntoMembershipPlanTypeTable @PlanType = 'Half-Yearly';
-
-EXEC spInsertDataIntoMembershipPlanTypeTable @PlanType = 'Yearly';
-
-SELECT *FROM tblMembershipPlanType
