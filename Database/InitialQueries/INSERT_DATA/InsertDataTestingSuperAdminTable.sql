87ed06082e62d9d17975e3256b70f26de37fe10b
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-06-02 23:40:39 +0530

    Update InsertDataTestingSuperAdminTable.sql

--- Database/InitialQueries/INSERT_DATA/InsertDataTestingSuperAdminTable.sql
+++ Database/InitialQueries/INSERT_DATA/InsertDataTestingSuperAdminTable.sql
@@ -1,19 +1,61 @@
 EXEC spInsertDataIntoSuperAdminTable
-@UserName='Admin1',
-@PasswordHash='Pass@101',
-@Email_Id='admin1@gmail.com',
-@PhoneNumber='9876543201'
+    @UserName = 'admin1',
+    @PasswordHash = 'Admin@123',
+    @EmailId = 'admin1@gmail.com',
+    @PhoneNumber = '9876543210';
 
 EXEC spInsertDataIntoSuperAdminTable
-@UserName='Admin2',
-@PasswordHash='Pass@102',
-@Email_Id='admin2@gmail.com',
-@PhoneNumber='9876543202'
+    @UserName = 'admin2',
+    @PasswordHash = 'Admin@124',
+    @EmailId = 'admin2@gmail.com',
+    @PhoneNumber = '9876543211';
 
 EXEC spInsertDataIntoSuperAdminTable
-@UserName='Admin3',
-@PasswordHash='Pass@103',
-@Email_Id='admin3@gmail.com',
-@PhoneNumber='9876543203'
+    @UserName = 'admin3',
+    @PasswordHash = 'Admin@125',
+    @EmailId = 'admin3@gmail.com',
+    @PhoneNumber = '9876543212';
+
+EXEC spInsertDataIntoSuperAdminTable
+    @UserName = 'admin4',
+    @PasswordHash = 'Admin@126',
+    @EmailId = 'admin4@gmail.com',
+    @PhoneNumber = '9876543213';
+
+EXEC spInsertDataIntoSuperAdminTable
+    @UserName = 'admin5',
+    @PasswordHash = 'Admin@127',
+    @EmailId = 'admin5@gmail.com',
+    @PhoneNumber = '9876543214';
+
+EXEC spInsertDataIntoSuperAdminTable
+    @UserName = 'admin6',
+    @PasswordHash = 'Admin@128',
+    @EmailId = 'admin6@gmail.com',
+    @PhoneNumber = '9876543215';
+
+EXEC spInsertDataIntoSuperAdminTable
+    @UserName = 'admin7',
+    @PasswordHash = 'Admin@129',
+    @EmailId = 'admin7@gmail.com',
+    @PhoneNumber = '9876543216';
+
+EXEC spInsertDataIntoSuperAdminTable
+    @UserName = 'admin8',
+    @PasswordHash = 'Admin@130',
+    @EmailId = 'admin8@gmail.com',
+    @PhoneNumber = '9876543217';
+
+EXEC spInsertDataIntoSuperAdminTable
+    @UserName = 'admin9',
+    @PasswordHash = 'Admin@131',
+    @EmailId = 'admin9@gmail.com',
+    @PhoneNumber = '9876543218';
+
+EXEC spInsertDataIntoSuperAdminTable
+    @UserName = 'admin10',
+    @PasswordHash = 'Admin@132',
+    @EmailId = 'admin10@gmail.com',
+    @PhoneNumber = '9876543219';
 
 SELECT *FROM tblSuperAdmin
