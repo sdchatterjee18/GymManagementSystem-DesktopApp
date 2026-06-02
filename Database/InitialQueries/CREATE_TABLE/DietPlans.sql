db8826e6154c1bb77fa49985d042322735389376
Arindam Sahoo <arindamsahoo302@gmail.com>
2026-06-02 16:30:26 +0530

    DROP THIS TABLE DIATEPLAIN

--- Database/InitialQueries/CREATE_TABLE/DietPlans.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE tblDietPlans(
-       DietPlanId INT PRIMARY KEY  NOT NULL IDENTITY(1,1),
-       CaloriesPerDay INT CHECK(CaloriesPerDay > 0) NOT NULL,
-       DietPlanDocument VARCHAR(MAX),
-       ConditionStatus VARCHAR(MAX) NOT NULL
-)
