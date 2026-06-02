b670a10f2e0b8e26e9cf30384bf2684af59a8aae
Priya Chowdhury <priyachowdhury528@gmail.com>
2026-06-02 23:55:50 +0530

    Create InsertDataMemberAttendanceTable  folder

---
+++ Database/InitialQueries/INSERT_DATA/InsertDataMemberAttendance/InsertDataTestingMemberAttendanceTable.sql
@@ -0,0 +1,16 @@
+EXEC spInsertDataIntoMemberAttendanceTable @MemberId = 1, @ShiftId = 1;
+EXEC spInsertDataIntoMemberAttendanceTable @MemberId = 2, @ShiftId = 2;
+EXEC spInsertDataIntoMemberAttendanceTable @MemberId = 3, @ShiftId = 3;
+EXEC spInsertDataIntoMemberAttendanceTable @MemberId = 4, @ShiftId = 1;
+EXEC spInsertDataIntoMemberAttendanceTable @MemberId = 5, @ShiftId = 2;
+EXEC spInsertDataIntoMemberAttendanceTable @MemberId = 6, @ShiftId = 3;
+EXEC spInsertDataIntoMemberAttendanceTable @MemberId = 7, @ShiftId = 1;
+EXEC spInsertDataIntoMemberAttendanceTable @MemberId = 8, @ShiftId = 2;
+EXEC spInsertDataIntoMemberAttendanceTable @MemberId = 9, @ShiftId = 3;
+EXEC spInsertDataIntoMemberAttendanceTable @MemberId = 10, @ShiftId = 1;
+
+EXEC spInsertDataIntoMemberAttendanceTable @MemberId = 1, @ShiftId = 2;
+EXEC spInsertDataIntoMemberAttendanceTable @MemberId = 2, @ShiftId = 3;
+EXEC spInsertDataIntoMemberAttendanceTable @MemberId = 3, @ShiftId = 1;
+EXEC spInsertDataIntoMemberAttendanceTable @MemberId = 4, @ShiftId = 2;
+EXEC spInsertDataIntoMemberAttendanceTable @MemberId = 5, @ShiftId = 3;
