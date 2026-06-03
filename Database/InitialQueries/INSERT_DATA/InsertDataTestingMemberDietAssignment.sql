9441673039b18716e53d09cf87533836cdd57ff7
Anshu_Dagur <164508557+Anshudagur@users.noreply.github.com>
2026-06-03 23:46:44 +0530

    Testing Data into TestingMemberDietAssignment Table

---
+++ Database/InitialQueries/INSERT_DATA/InsertDataTestingMemberDietAssignment.sql
@@ -0,0 +1,39 @@
+EXEC spInsertDataIntoMemberDietAssignmentTable 
+	@MemberId = 1, 
+	@DietPlanId = 3;
+
+EXEC spInsertDataIntoMemberDietAssignmentTable 
+	@MemberId = 2, 
+	@DietPlanId = 1;
+
+EXEC spInsertDataIntoMemberDietAssignmentTable 
+	@MemberId = 3, 
+	@DietPlanId = 5;
+
+EXEC spInsertDataIntoMemberDietAssignmentTable 
+	@MemberId = 4, 
+	@DietPlanId = 2;
+
+EXEC spInsertDataIntoMemberDietAssignmentTable 
+	@MemberId = 5, 
+	@DietPlanId = 6;
+
+EXEC spInsertDataIntoMemberDietAssignmentTable 
+	@MemberId = 6, 
+	@DietPlanId = 4;
+
+EXEC spInsertDataIntoMemberDietAssignmentTable 
+	@MemberId = 7, 
+	@DietPlanId = 9;
+
+EXEC spInsertDataIntoMemberDietAssignmentTable 
+	@MemberId = 8, 
+	@DietPlanId = 8;
+
+EXEC spInsertDataIntoMemberDietAssignmentTable 
+	@MemberId = 9, 
+	@DietPlanId = 7;
+
+EXEC spInsertDataIntoMemberDietAssignmentTable 
+	@MemberId = 10, 
+	@DietPlanId = 10;
