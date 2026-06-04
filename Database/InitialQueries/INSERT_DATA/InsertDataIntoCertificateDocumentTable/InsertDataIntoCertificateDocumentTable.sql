76121eb36b5f444cd2d821a9a7761689ed1eccfa
Santanu0007 <santanukhaskel2022@gmail.com>
2026-06-04 11:09:47 +0530

    Testing CertificateDocumentData

---
+++ Database/InitialQueries/INSERT_DATA/InsertDataIntoCertificateDocumentTable/InsertDataIntoCertificateDocumentTable.sql
@@ -0,0 +1,17 @@
+exec spInsertDataIntoCertificateDocumentTable
+@TrainerId=2,
+@Document= 010001010100101;
+
+exec spInsertDataIntoCertificateDocumentTable
+@TrainerId=3,
+@Document= 010001010100101;
+
+exec spInsertDataIntoCertificateDocumentTable
+@TrainerId=4,
+@Document= 010001010100101;
+
+exec spInsertDataIntoCertificateDocumentTable
+@TrainerId=5,
+@Document= 010001010100101;
+
+
