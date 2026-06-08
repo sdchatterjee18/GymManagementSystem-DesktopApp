7e8ee6d234ecaf9a6e833643a68e8c35259babcb
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-06-08 23:45:00 +0530

    StartDate Update

--- Database/InitialQueries/INSERT_DATA/InsertDataMembershipSubcription/InsertDataTestingMembershipSubscriptionTable.sql
+++ Database/InitialQueries/INSERT_DATA/InsertDataMembershipSubcription/InsertDataTestingMembershipSubscriptionTable.sql
@@ -1,59 +1,23 @@
 EXEC spInsertDataIntoMembershipSubscriptionTable
 @MemberId = 1,
 @MembershipPlanId = 1,
-@StartDate = '2026-06-03',
-@ExpiryDate = '2026-07-03';
+@StartDate = '2026-07-01',
+@ExpiryDate = '2026-08-01';
 
 EXEC spInsertDataIntoMembershipSubscriptionTable
 @MemberId = 2,
 @MembershipPlanId = 2,
-@StartDate = '2026-06-03',
-@ExpiryDate = '2026-07-03';
+@StartDate = '2026-07-01',
+@ExpiryDate = '2026-08-01';
 
 EXEC spInsertDataIntoMembershipSubscriptionTable
 @MemberId = 3,
 @MembershipPlanId = 3,
-@StartDate = '2026-06-03',
-@ExpiryDate = '2026-09-01';
+@StartDate = '2026-07-01',
+@ExpiryDate = '2026-09-29';
 
 EXEC spInsertDataIntoMembershipSubscriptionTable
 @MemberId = 4,
 @MembershipPlanId = 4,
-@StartDate = '2026-06-03',
-@ExpiryDate = '2027-06-03';
-
-EXEC spInsertDataIntoMembershipSubscriptionTable
-@MemberId = 5,
-@MembershipPlanId = 1,
-@StartDate = '2026-06-03',
-@ExpiryDate = '2026-07-03';
-
-EXEC spInsertDataIntoMembershipSubscriptionTable
-@MemberId = 6,
-@MembershipPlanId = 2,
-@StartDate = '2026-06-03',
-@ExpiryDate = '2026-07-03';
-
-EXEC spInsertDataIntoMembershipSubscriptionTable
-@MemberId = 7,
-@MembershipPlanId = 3,
-@StartDate = '2026-06-03',
-@ExpiryDate = '2026-09-01';
-
-EXEC spInsertDataIntoMembershipSubscriptionTable
-@MemberId = 8,
-@MembershipPlanId = 4,
-@StartDate = '2026-06-03',
-@ExpiryDate = '2027-06-03';
-
-EXEC spInsertDataIntoMembershipSubscriptionTable
-@MemberId = 9,
-@MembershipPlanId = 1,
-@StartDate = '2026-06-03',
-@ExpiryDate = '2026-07-03';
-
-EXEC spInsertDataIntoMembershipSubscriptionTable
-@MemberId = 10,
-@MembershipPlanId = 2,
-@StartDate = '2026-06-03',
-@ExpiryDate = '2026-07-03';
+@StartDate = '2026-07-01',
+@ExpiryDate = '2027-07-01';
