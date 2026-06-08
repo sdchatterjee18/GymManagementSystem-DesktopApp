624b24e00c8fbb89477c917990e8715c66e80d9d
Anshu_Dagur <164508557+Anshudagur@users.noreply.github.com>
2026-06-09 00:26:51 +0530

    spGetSubscriptionPaymentDetailsByMouth was updated

--- Database/InitialQueries/Store_Procedures/spGetSubscriptionPaymentDetailsByMouth.sql
+++ Database/InitialQueries/Store_Procedures/spGetSubscriptionPaymentDetailsByMouth.sql
@@ -2,13 +2,16 @@ CREATE PROC spGetSubscriptionPaymentDetailsByMonth
 @Month VARCHAR(50)
 AS
 BEGIN
+	SET NOCOUNT ON;
 	SET @Month = LTRIM(RTRIM(ISNULL(@Month,'')))
 	IF @Month <> '' AND @Month IN ('January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December')
 		BEGIN
 			SELECT 
 				s.TransactionId,
 				s.MemberId,
-				CONCAT(m.FirstName, ' ', m.MiddleName, ' ', m.LastName) AS MemberName,
+				CASE WHEN m.MiddleName IS NOT NULL THEN ' ' + m.MiddleName ELSE '' END,
+                    ' ', m.LastName
+                ) AS MemberName,
 				s.PaymentDate,
 				s.PaymentMethod,
 				s.Amount,
@@ -22,6 +25,7 @@ BEGIN
 				ON s.MemberId = m.MemberId
 			JOIN tblMembershipSubscription ms
 				ON  s.MemberId = ms.MemberId
+				AND s.MembershipPlanId = ms.MembershipPlanId
 			JOIN tblMembershipPlans mp
 				ON s.MembershipPlanId = mp.MembershipPlanId
 			WHERE FORMAT(s.PaymentDate, 'MMMM') = @Month
