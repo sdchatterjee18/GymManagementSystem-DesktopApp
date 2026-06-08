c4e549b2faf157b1c193b329346599dce8f8cb1d
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-06-09 00:04:42 +0530

    Update Employee proc

--- Database/InitialQueries/INSERT_DATA/InserDataEmployee/InsertDataTestingEmployeeTable.sql
+++ Database/InitialQueries/INSERT_DATA/InserDataEmployee/InsertDataTestingEmployeeTable.sql
@@ -1,101 +1,101 @@
 EXEC spInsertDataIntoEmployeeTable
-@FirstName='Rahul',
+@FirstName='Amit',
 @MiddleName='Kumar',
 @LastName='Sharma',
 @GenderId=1,
 @PhoneNo='9876543210',
+@EmailId='amit@gmail.com',
+@RoleId=1,
+@BankAccountNo='100000000001';
+
+EXEC spInsertDataIntoEmployeeTable
+@FirstName='Rahul',
+@MiddleName='',
+@LastName='Das',
+@GenderId=1,
+@PhoneNo='9876543211',
 @EmailId='rahul@gmail.com',
-@RoleName='Trainer',
-@BankAccountNo='123456789001'
+@RoleId=2,
+@BankAccountNo='100000000002';
 
 EXEC spInsertDataIntoEmployeeTable
 @FirstName='Sourav',
 @MiddleName='',
-@LastName='Ghosh',
+@LastName='Roy',
 @GenderId=1,
-@PhoneNo='9876543213',
+@PhoneNo='9876543212',
 @EmailId='sourav@gmail.com',
-@RoleName='Accountant',
-@BankAccountNo='123456789004'
+@RoleId=2,
+@BankAccountNo='100000000003';
 
 EXEC spInsertDataIntoEmployeeTable
-@FirstName='Neha',
+@FirstName='Arijit',
 @MiddleName='',
 @LastName='Paul',
-@GenderId=2,
-@PhoneNo='9876543216',
-@EmailId='neha@gmail.com',
-@RoleName='Manager',
-@BankAccountNo='123456789007'
+@GenderId=1,
+@PhoneNo='9876543213',
+@EmailId='arijit@gmail.com',
+@RoleId=2,
+@BankAccountNo='100000000004';
 
 EXEC spInsertDataIntoEmployeeTable
-@FirstName='Amit',
+@FirstName='Rakesh',
 @MiddleName='',
-@LastName='Das',
+@LastName='Ghosh',
 @GenderId=1,
-@PhoneNo='9876543220',
-@EmailId='amit@gmail.com',
-@RoleName='Trainer',
-@BankAccountNo='123456789010';
+@PhoneNo='9876543214',
+@EmailId='rakesh@gmail.com',
+@RoleId=2,
+@BankAccountNo='100000000005';
+
+EXEC spInsertDataIntoEmployeeTable
+@FirstName='Ankit',
+@MiddleName='',
+@LastName='Singh',
+@GenderId=1,
+@PhoneNo='9876543215',
+@EmailId='ankit@gmail.com',
+@RoleId=2,
+@BankAccountNo='100000000006';
 
 EXEC spInsertDataIntoEmployeeTable
 @FirstName='Priya',
 @MiddleName='',
 @LastName='Sen',
 @GenderId=2,
-@PhoneNo='9876543221',
+@PhoneNo='9876543216',
 @EmailId='priya@gmail.com',
-@RoleName='Receptionist',
-@BankAccountNo='123456789011';
+@RoleId=3,
+@BankAccountNo='100000000007';
 
 EXEC spInsertDataIntoEmployeeTable
-@FirstName='Rohit',
-@MiddleName='Kumar',
-@LastName='Roy',
-@GenderId=1,
-@PhoneNo='9876543222',
-@EmailId='rohit@gmail.com',
-@RoleName='Manager',
-@BankAccountNo='123456789012';
-
-EXEC spInsertDataIntoEmployeeTable
-@FirstName='Ananya',
+@FirstName='Mousumi',
 @MiddleName='',
-@LastName='Dutta',
+@LastName='Dey',
 @GenderId=2,
-@PhoneNo='9876543223',
-@EmailId='ananya@gmail.com',
-@RoleName='Trainer',
-@BankAccountNo='123456789013';
+@PhoneNo='9876543217',
+@EmailId='mousumi@gmail.com',
+@RoleId=3,
+@BankAccountNo='100000000008';
 
 EXEC spInsertDataIntoEmployeeTable
-@FirstName='Vikash',
+@FirstName='Puja',
 @MiddleName='',
-@LastName='Yadav',
-@GenderId=1,
-@PhoneNo='9876543224',
-@EmailId='vikash@gmail.com',
-@RoleName='Accountant',
-@BankAccountNo='123456789014';
-
-EXEC spInsertDataIntoEmployeeTable
-@FirstName='Pooja',
-@MiddleName='Rani',
-@LastName='Paul',
+@LastName='Mondal',
 @GenderId=2,
-@PhoneNo='9876543225',
-@EmailId='pooja@gmail.com',
-@RoleName='Receptionist',
-@BankAccountNo='123456789015';
+@PhoneNo='9876543218',
+@EmailId='puja@gmail.com',
+@RoleId=3,
+@BankAccountNo='100000000009';
 
 EXEC spInsertDataIntoEmployeeTable
-@FirstName='Arjun',
+@FirstName='Rina',
 @MiddleName='',
-@LastName='Singh',
-@GenderId=1,
-@PhoneNo='9876543226',
-@EmailId='arjun@gmail.com',
-@RoleName='Trainer',
-@BankAccountNo='123456789016';
+@LastName='Saha',
+@GenderId=2,
+@PhoneNo='9876543219',
+@EmailId='rina@gmail.com',
+@RoleId=3,
+@BankAccountNo='100000000010';
 
 SELECT *FROM tblEmployee
