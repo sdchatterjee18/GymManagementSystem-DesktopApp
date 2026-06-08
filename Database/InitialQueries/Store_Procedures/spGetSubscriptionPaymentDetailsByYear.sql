8e10710b3f926ec6f528baae8309bc9b401d1da3
Anshu_Dagur <164508557+Anshudagur@users.noreply.github.com>
2026-06-09 00:28:34 +0530

    Added Store Procedure GetSubscriptionPaymentDetailsByYear

---
+++ Database/InitialQueries/Store_Procedures/spGetSubscriptionPaymentDetailsByYear.sql
@@ -0,0 +1,38 @@
+CREATE PROC spGetSubscriptionPaymentDetailsByYear
+    @Year INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    IF @Year IS NOT NULL
+        BEGIN
+            SELECT 
+                s.TransactionId,
+                s.MemberId,
+                CONCAT(
+                    m.FirstName, 
+                    CASE WHEN m.MiddleName IS NOT NULL THEN ' ' + m.MiddleName ELSE '' END,
+                    ' ', m.LastName
+                ) AS MemberName,
+                s.PaymentDate,
+                s.PaymentMethod,
+                s.Amount,
+                m.Email_Id,
+                mp.MembershipPlanName,
+                ms.StartDate,
+                ms.ExpiryDate,
+                s.FeesType
+            FROM tblSubscriptionPayment s 
+            JOIN tblMember m
+                ON s.MemberId = m.MemberId
+            JOIN tblMembershipSubscription ms
+                ON s.MemberId = ms.MemberId
+                AND s.MembershipPlanId = ms.MembershipPlanId
+            JOIN tblMembershipPlans mp
+                ON s.MembershipPlanId = mp.MembershipPlanId
+            WHERE YEAR(s.PaymentDate) = @Year
+        END
+    ELSE
+        BEGIN
+            SELECT 'Year is required.' AS Message
+        END
+END
