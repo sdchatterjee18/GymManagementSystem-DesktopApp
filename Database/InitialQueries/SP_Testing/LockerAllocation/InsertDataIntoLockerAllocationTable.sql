b0fccb1bc78205b548aabe1f0c8c180e724ef0ef
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-06-09 23:42:54 +0530

    Update InsertDataIntoLockerAllocationTable.sql

--- Database/InitialQueries/SP_Testing/LockerAllocation/InsertDataIntoLockerAllocationTable.sql
+++ Database/InitialQueries/SP_Testing/LockerAllocation/InsertDataIntoLockerAllocationTable.sql
@@ -1,27 +1,24 @@
 CREATE PROC spInsertDataIntoLockerAllocationTable
 (
     @LockerId INT,
-    @MemberId INT,
-    @ExpiryDate DATE
+    @MemberId INT
 )
 AS
 BEGIN
 BEGIN TRY
+
     IF @LockerId IS NULL
     BEGIN
         SELECT 'Locker Id is Required.' AS Message
         RETURN
     END
+
     IF @MemberId IS NULL
     BEGIN
         SELECT 'Member Id is Required.' AS Message
         RETURN
     END
-    IF @ExpiryDate IS NULL
-    BEGIN
-        SELECT 'Expiry Date is Required.' AS Message
-        RETURN
-    END
+
     IF NOT EXISTS
     (
         SELECT 1
@@ -32,6 +29,7 @@ BEGIN TRY
         SELECT 'Invalid Locker Id.' AS Message
         RETURN
     END
+
     IF NOT EXISTS
     (
         SELECT 1
@@ -43,46 +41,45 @@ BEGIN TRY
         RETURN
     END
 
-    IF @ExpiryDate <= CAST(GETDATE() AS DATE)
-    BEGIN
-        SELECT 'Expiry Date Must Be Greater Than Today.' AS Message
-        RETURN
-    END
     IF EXISTS
     (
         SELECT 1
         FROM tblLockerAllocation
         WHERE LockerId = @LockerId
-          AND ExpiryDate >= CAST(GETDATE() AS DATE)
     )
     BEGIN
         SELECT 'Locker Is Already Allocated To A Member.' AS Message
         RETURN
     END
+
     IF EXISTS
     (
         SELECT 1
         FROM tblLockerAllocation
         WHERE MemberId = @MemberId
-          AND ExpiryDate >= CAST(GETDATE() AS DATE)
     )
     BEGIN
         SELECT 'Member Already Has An Active Locker Allocation.' AS Message
         RETURN
     END
+
     INSERT INTO tblLockerAllocation
     (
         LockerId,
-        MemberId,
-        ExpiryDate
+        MemberId
     )
     VALUES
     (
         @LockerId,
-        @MemberId,
-        @ExpiryDate
+        @MemberId
     )
+
+    UPDATE tblLocker
+    SET LockerStatus = 'Occupied'
+    WHERE LockerId = @LockerId;
+
     SELECT 'Locker Allocated Successfully.' AS Message
+
 END TRY
 BEGIN CATCH
 
