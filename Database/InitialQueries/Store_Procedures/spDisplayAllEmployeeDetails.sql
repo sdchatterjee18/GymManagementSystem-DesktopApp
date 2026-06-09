5242c6977c41a8ceb86ea871f0b1768596599b4e
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-06-09 23:09:04 +0530

    spDisplayAllEmployeeDetails SP added

---
+++ Database/InitialQueries/Store_Procedures/spDisplayAllEmployeeDetails.sql
@@ -0,0 +1,26 @@
+CREATE PROC spDisplayAllEmployeeDetails
+AS
+BEGIN
+
+    SELECT
+        E.EmployeeId,
+        E.FirstName,
+        E.MiddleName,
+        E.LastName,
+        G.GenderName,
+        E.PhoneNo,
+        E.EmailId,
+        E.JoiningDate,
+        E.IsActive,
+        R.Role AS RoleName,
+        E.BankAccountNo,
+        S.Amount AS Salary
+    FROM tblEmployee E
+    INNER JOIN tblGender G
+        ON E.GenderId = G.GenderId
+    INNER JOIN tblEmployeeRoleType R
+        ON E.RoleId = R.RoleId
+    INNER JOIN tblSalary S
+        ON E.EmployeeId = S.EmployeeId
+    ORDER BY E.EmployeeId;
+END
