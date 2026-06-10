0317e47087db0d30f613345d3c1400f31defba28
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-06-10 12:30:18 +0530

    Update

--- Database/InitialQueries/CREATE_TABLE/✔️CertificateDocument.sql
+++ Database/InitialQueries/CREATE_TABLE/✔️CertificateDocument.sql
@@ -2,7 +2,7 @@ CREATE TABLE tblCertificateDocument
 (
 	DocumentId INT PRIMARY KEY NOT NULL IDENTITY(1,1),
 	TrainerId INT NOT NULL,
-	Document VARBINARY(MAX) NOT NULL,
+	Document VARBINARY(MAX),
 
 	CONSTRAINT FK_tblCertificateDocument_TrainerIdtblTrainer
 	FOREIGN KEY (TrainerId)
