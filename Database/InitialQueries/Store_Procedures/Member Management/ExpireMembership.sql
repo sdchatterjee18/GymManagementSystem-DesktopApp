ff0fc02fc4067e8706c0162ac1f01ff44fc238a8
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-07-04 23:29:43 +0530

    Create ExpireMembership.sql

---
+++ Database/InitialQueries/Store_Procedures/Member Management/ExpireMembership.sql
@@ -0,0 +1,63 @@
+CREATE PROC spExpireMembership
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+
+        BEGIN TRANSACTION;
+
+        ---------------------------------------------------------
+        -- Deactivate Expired Membership
+        ---------------------------------------------------------
+        UPDATE tblMembershipSubscription
+        SET IsActive = 0
+        WHERE IsActive = 1
+          AND ExpiryDate < CAST(GETDATE() AS DATE);
+
+        ---------------------------------------------------------
+        -- Deactivate Member Trainer Assignment
+        ---------------------------------------------------------
+        UPDATE MTA
+        SET MTA.IsActive = 0
+        FROM tblMemberTrainerAssignment MTA
+        INNER JOIN tblMembershipSubscription MS
+            ON MTA.MemberId = MS.MemberId
+        WHERE MTA.IsActive = 1
+          AND MS.IsActive = 0;
+
+        ---------------------------------------------------------
+        -- Deactivate Trainer Shift
+        ---------------------------------------------------------
+        UPDATE TS
+        SET TS.IsActive = 0
+        FROM tblTrainerShift TS
+        INNER JOIN tblMemberTrainerAssignment MTA
+            ON TS.TrainerId = MTA.TrainerId
+        INNER JOIN tblMembershipSubscription MS
+            ON MTA.MemberId = MS.MemberId
+        WHERE TS.IsActive = 1
+          AND MTA.IsActive = 0
+          AND MS.IsActive = 0;
+
+        COMMIT TRANSACTION;
+
+        SELECT
+            1 AS Success,
+            'Expired memberships processed successfully.' AS Message;
+
+    END TRY
+    BEGIN CATCH
+
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+
+        SELECT
+            0 AS Success,
+            ERROR_MESSAGE() AS Message,
+            ERROR_LINE() AS ErrorLine,
+            ERROR_PROCEDURE() AS ProcedureName;
+
+    END CATCH
+END
+GO
