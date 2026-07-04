86c861960a6c1c60ace6e8e0da02d4baa9760c3c
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-07-04 12:44:53 +0530

    Update ExecuteAssignTrainerToMember.sql

--- Database/InitialQueries/Store_Procedures/Member Trainer Management/Assign Trainer/ExecuteAssignTrainerToMember.sql
+++ Database/InitialQueries/Store_Procedures/Member Trainer Management/Assign Trainer/ExecuteAssignTrainerToMember.sql
@@ -1,39 +1,11 @@
-EXEC spAssignTrainerToMember
-    @MemberId = 1,
-    @TrainerId = 1;
+EXEC spAssignPersonalTrainerToMember @MemberId = 1,  @TrainerId = 1;
+EXEC spAssignPersonalTrainerToMember @MemberId = 2,  @TrainerId = 2;
+EXEC spAssignPersonalTrainerToMember @MemberId = 3,  @TrainerId = 3;
+EXEC spAssignPersonalTrainerToMember @MemberId = 4,  @TrainerId = 4;
+EXEC spAssignPersonalTrainerToMember @MemberId = 5,  @TrainerId = 5;
 
-EXEC spAssignTrainerToMember
-    @MemberId = 2,
-    @TrainerId = 2;
-
-EXEC spAssignTrainerToMember
-    @MemberId = 3,
-    @TrainerId = 3;
-
-EXEC spAssignTrainerToMember
-    @MemberId = 4,
-    @TrainerId = 4;
-
-EXEC spAssignTrainerToMember
-    @MemberId = 5,
-    @TrainerId = 5;
-
-EXEC spAssignTrainerToMember
-    @MemberId = 6,
-    @TrainerId = 1;
-
-EXEC spAssignTrainerToMember
-    @MemberId = 7,
-    @TrainerId = 2;
-
-EXEC spAssignTrainerToMember
-    @MemberId = 8,
-    @TrainerId = 3;
-
-EXEC spAssignTrainerToMember
-    @MemberId = 9,
-    @TrainerId = 4;
-
-EXEC spAssignTrainerToMember
-    @MemberId = 10,
-    @TrainerId = 5;
+EXEC spAssignPersonalTrainerToMember @MemberId = 6,  @TrainerId = 1;
+EXEC spAssignPersonalTrainerToMember @MemberId = 7,  @TrainerId = 2;
+EXEC spAssignPersonalTrainerToMember @MemberId = 8,  @TrainerId = 3;
+EXEC spAssignPersonalTrainerToMember @MemberId = 9,  @TrainerId = 4;
+EXEC spAssignPersonalTrainerToMember @MemberId = 10, @TrainerId = 5;
