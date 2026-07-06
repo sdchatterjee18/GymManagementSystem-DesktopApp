617a8dda02754e786d774b6675caf086b4731c54
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-07-06 22:35:59 +0530

    Update InsertDietPlan.sql

--- Database/InitialQueries/Store_Procedures/DietManagement/InsertDietPlan.sql
+++ Database/InitialQueries/Store_Procedures/DietManagement/InsertDietPlan.sql
@@ -60,9 +60,7 @@ END TRY
 BEGIN CATCH
 
     SELECT
-        ERROR_MESSAGE() AS Message,
-        ERROR_LINE() AS ErrorLine,
-        ERROR_PROCEDURE() AS ProcedureName;
+        ERROR_MESSAGE() AS Message;
 
 END CATCH
 END;
