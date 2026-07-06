2803be27f7395e7fddedf929a4120a75122ef777
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-07-06 22:15:12 +0530

    Update RetrieveMonthlyTotalExpense.sql

--- Database/InitialQueries/Store_Procedures/ExpenseManagement/RetrieveMonthlyTotalExpense.sql
+++ Database/InitialQueries/Store_Procedures/ExpenseManagement/RetrieveMonthlyTotalExpense.sql
@@ -1,4 +1,4 @@
-CREATE OR ALTER PROC spRetrieveMonthlyTotalExpense
+CREATE PROC spRetrieveMonthlyTotalExpense
 (
     @Month INT,
     @Year INT
@@ -7,54 +7,34 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    BEGIN TRY
+    -------------------------------------------------
+    -- Month Validation
+    -------------------------------------------------
+    IF @Month IS NULL
+       OR @Month NOT BETWEEN 1 AND 12
+    BEGIN
+        SELECT 'Invalid Month.' AS Message;
+        RETURN;
+    END;
 
-        -------------------------------------------------
-        -- Month Validation
-        -------------------------------------------------
-        IF @Month IS NULL
-           OR @Month NOT BETWEEN 1 AND 12
-        BEGIN
-            SELECT
-                0 AS Success,
-                'Invalid Month.' AS Message;
-            RETURN;
-        END;
+    -------------------------------------------------
+    -- Year Validation
+    -------------------------------------------------
+    IF @Year IS NULL
+       OR @Year < 2000
+       OR @Year > YEAR(GETDATE())
+    BEGIN
+        SELECT 'Invalid Year.' AS Message;
+        RETURN;
+    END;
 
-        -------------------------------------------------
-        -- Year Validation
-        -------------------------------------------------
-        IF @Year IS NULL
-           OR @Year > YEAR(GETDATE())
-        BEGIN
-            SELECT
-                0 AS Success,
-                'Invalid Year.' AS Message;
-            RETURN;
-        END;
-
-        -------------------------------------------------
-        -- Total Monthly Expense
-        -------------------------------------------------
-        SELECT
-            1 AS Success,
-            @Month AS [Month],
-            @Year AS [Year],
-            ISNULL(SUM(ExpenseAmount), 0) AS TotalMonthlyExpense
-        FROM tblExpense
-        WHERE MONTH(ExpenseDate) = @Month
-          AND YEAR(ExpenseDate) = @Year;
-
-    END TRY
-
-    BEGIN CATCH
-
-        SELECT
-            0 AS Success,
-            ERROR_MESSAGE() AS Message,
-            ERROR_LINE() AS ErrorLine,
-            ERROR_PROCEDURE() AS ProcedureName;
-
-    END CATCH
+    -------------------------------------------------
+    -- Total Monthly Expense
+    -------------------------------------------------
+    SELECT
+        ISNULL(SUM(ExpenseAmount), 0) AS TotalMonthlyExpense
+    FROM tblExpense
+    WHERE ExpenseDate >= DATEFROMPARTS(@Year, @Month, 1)
+      AND ExpenseDate < DATEADD(MONTH, 1, DATEFROMPARTS(@Year, @Month, 1));
 END;
 GO
