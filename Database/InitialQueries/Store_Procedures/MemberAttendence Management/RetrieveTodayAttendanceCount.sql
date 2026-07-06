716d835eb9653e87442d66e2aa4edf27a561b756
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-07-06 23:22:40 +0530

    Update RetrieveTodayAttendanceCount.sql

--- Database/InitialQueries/Store_Procedures/MemberAttendence Management/RetrieveTodayAttendanceCount.sql
+++ Database/InitialQueries/Store_Procedures/MemberAttendence Management/RetrieveTodayAttendanceCount.sql
@@ -1,12 +1,23 @@
-CREATE PROC spRetrieveTodayAttendanceCount
+CREATE OR ALTER PROC spRetrieveTodayAttendanceCount
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT
-        COUNT(MemberId) AS TodayAttendanceCount
-    FROM tblMemberAttendance
-    WHERE AttendanceDate >= CAST(GETDATE() AS DATE)
-      AND AttendanceDate < DATEADD(DAY, 1, CAST(GETDATE() AS DATE));
+    BEGIN TRY
+
+        SELECT
+            COUNT(MemberId) AS TodayAttendanceCount
+        FROM tblMemberAttendance
+        WHERE AttendanceDate >= CAST(GETDATE() AS DATE)
+          AND AttendanceDate < DATEADD(DAY, 1, CAST(GETDATE() AS DATE));
+
+    END TRY
+
+    BEGIN CATCH
+
+        SELECT ERROR_MESSAGE() AS Message;
+
+    END CATCH
+
 END;
 GO
