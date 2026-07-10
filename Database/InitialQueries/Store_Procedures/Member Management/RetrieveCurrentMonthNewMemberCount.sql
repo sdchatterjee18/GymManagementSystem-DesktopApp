82f93dfe5ff7933ff2ce946c5a37241aa0eff67a
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-07-11 02:29:27 +0530

    Create RetrieveCurrentMonthNewMemberCount.sql

---
+++ Database/InitialQueries/Store_Procedures/Member Management/RetrieveCurrentMonthNewMemberCount.sql
@@ -0,0 +1,25 @@
+CREATE PROC spRetrieveCurrentMonthNewMemberCount
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+
+        SELECT
+            COUNT(MemberId) AS TotalNewMembers
+        FROM tblMember
+        WHERE
+            MONTH(JoiningDate) = MONTH(GETDATE())
+            AND YEAR(JoiningDate) = YEAR(GETDATE())
+            AND IsActive = 1;
+
+    END TRY
+    BEGIN CATCH
+
+        SELECT
+            ERROR_NUMBER() AS ErrorNumber,
+            ERROR_MESSAGE() AS ErrorMessage;
+
+    END CATCH
+END;
+GO
