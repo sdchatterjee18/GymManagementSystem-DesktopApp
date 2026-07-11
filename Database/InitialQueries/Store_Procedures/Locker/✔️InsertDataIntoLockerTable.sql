b842cad1729bc5d2dfc0e763becde1b147f0df47
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-07-11 12:24:28 +0530

    Update ✔️InsertDataIntoLockerTable.sql

--- Database/InitialQueries/Store_Procedures/Locker/✔️InsertDataIntoLockerTable.sql
+++ Database/InitialQueries/Store_Procedures/Locker/✔️InsertDataIntoLockerTable.sql
@@ -1,48 +1,57 @@
 CREATE PROC spInsertDataIntoLockerTable
 (
-    @LockerNo VARCHAR(20),
-    @LockerStatus VARCHAR(50)
+    @LockerNo VARCHAR(20)
 )
 AS
 BEGIN
-BEGIN TRY
-    SET @LockerNo = LTRIM(RTRIM(@LockerNo));
-    SET @LockerStatus = LTRIM(RTRIM(@LockerStatus));
-    IF @LockerNo IS NULL OR @LockerNo = ''
-    BEGIN
-        SELECT 'Locker Number is Required.' AS Message;
-        RETURN;
-    END
-    IF @LockerStatus IS NULL OR @LockerStatus = ''
-    BEGIN
-        SELECT 'Locker Status is Required.' AS Message;
-        RETURN;
-    END
-    IF EXISTS
-    (
-        SELECT 1
-        FROM tblLocker
-        WHERE LockerNo = @LockerNo
-    )
-    BEGIN
-        SELECT 'Locker Number Already Exists.' AS Message;
-        RETURN;
-    END
-    INSERT INTO tblLocker
-    (
-        LockerNo,
-        LockerStatus
-    )
-    VALUES
-    (
-        @LockerNo,
-        @LockerStatus
-    );
-    SELECT 'Locker Inserted Successfully.' AS Message;
-END TRY
-BEGIN CATCH
-    SELECT
-        ERROR_MESSAGE() AS Message;
-END CATCH
-END
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+
+        SET @LockerNo = LTRIM(RTRIM(@LockerNo));
+
+        -----------------------------------------
+        -- Locker Number Validation
+        -----------------------------------------
+        IF @LockerNo IS NULL OR @LockerNo = ''
+        BEGIN
+            SELECT 'Locker Number is Required.' AS Message;
+            RETURN;
+        END;
+
+        -----------------------------------------
+        -- Duplicate Locker Check
+        -----------------------------------------
+        IF EXISTS
+        (
+            SELECT 1
+            FROM tblLocker
+            WHERE LockerNo = @LockerNo
+        )
+        BEGIN
+            SELECT 'Locker Number Already Exists.' AS Message;
+            RETURN;
+        END;
+
+        -----------------------------------------
+        -- Insert Locker
+        -----------------------------------------
+        INSERT INTO tblLocker
+        (
+            LockerNo,
+            LockerStatus
+        )
+        VALUES
+        (
+            @LockerNo,
+            'Available'
+        );
+
+        SELECT 'Locker Inserted Successfully.' AS Message;
+
+    END TRY
+    BEGIN CATCH
+        SELECT ERROR_MESSAGE() AS Message;
+    END CATCH
+END;
 GO
