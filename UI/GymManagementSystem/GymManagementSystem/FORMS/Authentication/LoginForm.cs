76bbea3256b7d8c060aa91629157022b38afd3bd
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-07-11 18:26:43 +0530

    UI design folder structred and forms added(Empty)

---
+++ UI/GymManagementSystem/GymManagementSystem/FORMS/Authentication/LoginForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GymManagementSystem.FORMS.Login
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
