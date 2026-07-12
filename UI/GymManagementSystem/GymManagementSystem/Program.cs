7cf9863b71717e08b4e435550ad336e079080ee4
Priya Chowdhury <priyachowdhury528@gmail.com>
2026-07-12 23:17:45 +0530

    Update Registration Fees

--- UI/GymManagementSystem/GymManagementSystem/Program.cs
+++ UI/GymManagementSystem/GymManagementSystem/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using GymManagementSystem.FORMS.RegistrationFees;
 
 namespace GymManagementSystem
 {
@@ -16,7 +15,7 @@ namespace GymManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new RegFeesForm());
+            Application.Run(new Form1());
         }
     }
 }
