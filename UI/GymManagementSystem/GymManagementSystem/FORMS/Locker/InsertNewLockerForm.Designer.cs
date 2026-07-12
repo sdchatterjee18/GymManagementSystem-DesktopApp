910967a8862cb9bb001a775a1d783dde192892a1
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-07-12 17:17:50 +0530

    Locker folder added with empty forms

---
+++ UI/GymManagementSystem/GymManagementSystem/FORMS/Locker/InsertNewLockerForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace GymManagementSystem.FORMS.Locker
+{
+    partial class InsertNewLockerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // InsertNewLockerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "InsertNewLockerForm";
+            this.Text = "InsertNewLockerForm";
+            this.Load += new System.EventHandler(this.InsertNewLockerForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ UI/GymManagementSystem/GymManagementSystem/FORMS/Locker/InsertNewLockerForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GymManagementSystem.FORMS.Locker
+{
+    public partial class InsertNewLockerForm : Form
+    {
+        public InsertNewLockerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InsertNewLockerForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
