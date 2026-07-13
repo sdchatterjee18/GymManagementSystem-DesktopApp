f9dad665a59cb63cbce6a8de31cfe9c045ece682
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-07-14 01:53:06 +0530

    designer files were added

--- UserInterface/GymManagementSystem/GymManagementSystem/FORMS/Attendance/FrmMarkAttendance.Designer.cs
+++ UserInterface/GymManagementSystem/GymManagementSystem/FORMS/Attendance/FrmMarkAttendance.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmMarkAttendance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "FrmMarkAttendance";
             this.Text = "FrmMarkAttendance";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- UserInterface/GymManagementSystem/GymManagementSystem/FORMS/Locker/FrmAddLocker.Designer.cs
+++ UserInterface/GymManagementSystem/GymManagementSystem/FORMS/Locker/FrmAddLocker.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmAddLocker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "FrmAddLocker";
             this.Text = "FrmAddLocker";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
