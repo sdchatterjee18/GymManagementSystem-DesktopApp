f42c939b2dc908a77e0b243a5c1bd653897d3aee
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-07-14 01:40:33 +0530

    Forms added in the solution

---
+++ UserInterface/GymManagementSystem/GymManagementSystem/FORMS/DietPlan/FrmShowDietPlans.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GymManagementSystem.FORMS.DietPlan
+{
+    public partial class FrmShowDietPlans : Form
+    {
+        public FrmShowDietPlans()
+        {
+            InitializeComponent();
+        }
+    }
+}
