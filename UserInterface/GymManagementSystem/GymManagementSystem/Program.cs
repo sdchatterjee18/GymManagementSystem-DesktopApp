18da9bdda1e07564f65497c4d22f40a556fa9844
deepjyotidas21062005-design <deepjyotidas21062005@gmail.com>
2026-07-20 15:49:51 +0530

    dashboard form design completed

--- UserInterface/GymManagementSystem/GymManagementSystem/Program.cs
+++ UserInterface/GymManagementSystem/GymManagementSystem/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using GymManagementSystem.FORMS.Dashboard;
 
 namespace GymManagementSystem
 {
@@ -15,7 +16,7 @@ namespace GymManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmDashboard());
         }
     }
 }
