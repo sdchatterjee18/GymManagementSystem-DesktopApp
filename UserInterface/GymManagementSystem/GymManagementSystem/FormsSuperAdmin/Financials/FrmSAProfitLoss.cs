a9c67f7fef99691447531e0bd5e962bfd1b01067
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-07-23 23:33:17 +0530

    Super Admin All Forms created (Empty)

---
+++ UserInterface/GymManagementSystem/GymManagementSystem/FormsSuperAdmin/Financials/FrmSAProfitLoss.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GymManagementSystem.FormsSuperAdmin.Financials
+{
+    public partial class FrmSAProfitLoss : Form
+    {
+        public FrmSAProfitLoss()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UserInterface/GymManagementSystem/GymManagementSystem/FormsSuperAdmin/Profile/FrmSAProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GymManagementSystem.FormsSuperAdmin.Profile
+{
+    public partial class FrmSAProfile : Form
+    {
+        public FrmSAProfile()
+        {
+            InitializeComponent();
+        }
+    }
+}
