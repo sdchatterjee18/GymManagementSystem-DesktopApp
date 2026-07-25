e605a3ee57f2a8327aec3c0574b71e5bd2c62d49
Anshu_Dagur <164508557+Anshudagur@users.noreply.github.com>
2026-07-25 10:35:02 +0530

    Design (changed) assigned the Form1 to open

--- UserInterface/GymManagementSystem/GymManagementSystem/Program.cs
+++ UserInterface/GymManagementSystem/GymManagementSystem/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using GymManagementSystem.FORMS.Locker;
 
 namespace GymManagementSystem
 {
@@ -16,7 +15,7 @@ namespace GymManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmDisplayLocker());
+            Application.Run(new Form1());
         }
     }
 }
