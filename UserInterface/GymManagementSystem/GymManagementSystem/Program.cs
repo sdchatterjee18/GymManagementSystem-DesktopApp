c0e3d4c626b565d55fc4f58a188b45cbed5dead5
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-07-26 13:40:32 +0530

    FrmmemberTrainerAssignment integrated

--- UserInterface/GymManagementSystem/GymManagementSystem/Program.cs
+++ UserInterface/GymManagementSystem/GymManagementSystem/Program.cs
@@ -21,7 +21,7 @@ namespace GymManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmMemberTrainerAssignment());
+            Application.Run(new FrmMainLayout());
         }
     }
 }
