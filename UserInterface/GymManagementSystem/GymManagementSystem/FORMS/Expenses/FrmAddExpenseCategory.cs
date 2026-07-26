7b4144896fcbe8c36dd3453761696520ffe2b9a5
Santanu0007 <santanukhaskel2022@gmail.com>
2026-07-26 21:38:04 +0530

    Expenses all form design is done

--- UserInterface/GymManagementSystem/GymManagementSystem/FORMS/Expenses/FrmAddExpenseCategory.cs
+++ UserInterface/GymManagementSystem/GymManagementSystem/FORMS/Expenses/FrmAddExpenseCategory.cs
@@ -15,5 +15,62 @@ namespace GymManagementSystem.FORMS.Expenses
         {
             InitializeComponent();
         }
+
+        private void tlpSubmit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblClear_Click(object sender, EventArgs e)
+        {
+            txtRequiredCategoryName.Clear();
+            txtInputCategory.Clear();
+        }
+
+        private void txtRequiredCategoryName_Click(object sender, EventArgs e)
+        {
+            int CategoryName = 1;
+            if (CategoryName == 1)
+            {
+                txtRequiredCategoryName.Clear();
+                txtRequiredCategoryName.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtInputCategory_Click(object sender, EventArgs e)
+        {
+            int Category = 1;
+            if (Category == 1)
+            {
+                txtInputCategory.Clear();
+                txtInputCategory.ForeColor = Color.Black;
+            }
+        }
+
+        private void FrmAddExpenseCategory_Load(object sender, EventArgs e)
+        {
+            txtInputCategory.Select(0, 0);
+            txtInputCategory.DeselectAll();
+
+            txtRequiredCategoryName.Select(0, 0);
+            txtRequiredCategoryName.DeselectAll();
+        }
+
+        private void lblCancel_MouseEnter(object sender, EventArgs e)
+        {
+            lblCancel.BackColor = Color.FromArgb(196, 43, 28);
+            lblCancel.ForeColor = Color.White;
+        }
+
+        private void lblCancel_MouseLeave(object sender, EventArgs e)
+        {
+            lblCancel.BackColor = Color.FromArgb(250,250,250);
+            lblCancel.ForeColor = Color.Black;
+        }
+
+        private void lblCancel_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
     }
 }
