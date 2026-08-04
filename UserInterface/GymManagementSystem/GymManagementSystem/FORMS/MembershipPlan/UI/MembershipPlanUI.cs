6efbf62f06d76848c82cc4023f299f111ba5e193
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-08-04 23:25:35 +0530

    MembershipPlan Module created (All empty)

---
+++ UserInterface/GymManagementSystem/GymManagementSystem/FORMS/MembershipPlan/UI/MembershipPlanUI.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GymManagementSystem.FORMS.MembershipPlan.UI
+{
+    class MembershipPlanUI
+    {
+    }
+}
---
+++ UserInterface/GymManagementSystem/GymManagementSystemDALayer/ModulesDALayer/MembershipPlan/MembershipPlanDAL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GymManagementSystemDALayer.ModulesDALayer.MembershipPlan
+{
+    class MembershipPlanDAL
+    {
+    }
+}
