b93b612a381012ba176dc946cf6e7db2fd495bb1
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-08-12 14:57:30 +0530

    Update ✔️Expenses.sql

--- Database/InitialQueries/CREATE_TABLE/✔️Expenses.sql
+++ Database/InitialQueries/CREATE_TABLE/✔️Expenses.sql
@@ -4,7 +4,7 @@ CREATE TABLE tblExpense
     ExpenseCategoryId INT NOT NULL,
     ExpenseAmount DECIMAL(10,2) NOT NULL
         CHECK (ExpenseAmount >= 0),
-    ExpenseDate DATE NOT NULL,
+    ExpenseDate DATETIME NOT NULL,
     Notes VARCHAR(MAX) NULL,
     CONSTRAINT FK_tblExpense_ExpenseCategoryIdtblExpenseCategory
         FOREIGN KEY (ExpenseCategoryId)
