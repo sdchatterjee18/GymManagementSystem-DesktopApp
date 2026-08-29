743e0db1e79a91ca429b5da094c49204a2a8f1b8
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-08-29 08:59:34 +0530

    ForgotPassword empty  form changed

---
+++ UserInterface/GymManagementSystem/GymManagementSystem/Authentication/FrmForgotPassword.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GymManagementSystem.Authentication
+{
+    public partial class FrmForgotPassword : Form
+    {
+        public FrmForgotPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmForgotPassword_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
